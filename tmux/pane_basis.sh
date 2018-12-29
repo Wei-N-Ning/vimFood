3d7637a60c057bef4fd2a1a6313c72d663ac9ab2
Wei Ning <nwcgnw@gmail.com>
2018-12-29 15:04:48 +1100

    init tmux

---
+++ tmux/pane_basis.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# source
+# https://hackernoon.com/a-gentle-introduction-to-tmux-8d784c404340
+
+# cb: CTRL + b
+
+: '
+to split a pane horizontally
+cb "
+
+to... vertically
+cb %
+
+to move:
+cb <arrow key>
+'
+
+: '
+use theme
+
+source
+https://github.com/jimeh/tmux-themepack/blob/master/powerline/default/cyan.tmuxtheme
+
+
+'
